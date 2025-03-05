68756090211a6474698c0c67f524a21263c7882c
yjh@yjh-virtual-machine:~$ <yjh@yjh-virtual-machine:~$>
2025-03-05 14:00:35 +0900

    hello

--- c_example/limit.c
+++ c_example/limit.c
@@ -9,11 +9,13 @@ int main(void)
     printHello();
     printf("char_bit : %d\n",CHAR_BIT);
     printf("char_max : %d\n", CHAR_MAX);
+    printf("INT_MIN : %d \t INT_MIN : %d", INT_MIN, INT_MAX);
+    printf("LONG_MIN : %d \t LONG_MAX: %ld", LONG_MIN,LONG_MAX);
+    printf("INT_MAX +1 : %d\n", INT_MAX +1);
     return 0;
 }
 
 void printHello()
 {
     printf("hello world\n");
-    return 0;
 }
