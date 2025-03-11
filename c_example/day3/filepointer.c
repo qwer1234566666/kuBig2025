966f95f5cd4ca79daf5ac8a0b3c8a52d14a6c2ef
yjh@yjh-virtual-machine:~$ <yjh@yjh-virtual-machine:~$>
2025-03-11 17:28:14 +0900

    hello

---
+++ c_example/day3/filepointer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    FILE *a;
+    a= fopen("test.dat", "w");
+    if( a== NULL){
+        printf("can't open file\n");
+        return 1;
+    }
+    fprintf(a, "이것은 파일로 저장된 예시 데이터 입니다.");
+    fclose(a);
+    return 0;
+}
