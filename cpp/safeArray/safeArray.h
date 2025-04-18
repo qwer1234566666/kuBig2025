019883cd3d13a0975e75da72a65caae19eed8c16
qwer1234566666 <dbwlsgh612859@gmail.com>
2025-04-18 11:32:40 +0900

    SafeArraycpp

---
+++ cpp/safeArray/safeArray.h
@@ -0,0 +1,20 @@
+#ifndef SAFE_ARRAY_H
+#define SAFE_ARRAY_H
+#include "array.h"
+
+class SafeArray : public Array {
+private:
+
+public:
+explicit SafeArray(int size = Array::ARRAY_SIZE);
+    SafeArray(const int *pArr, int size);
+    SafeArray(const SafeArray& rhs);
+    SafeArray& operator=(const SafeArray& );
+
+    bool operator==(const SafeArray& rhs) const;
+
+    virtual int& operator[](int index);
+    virtual const int& operator[](int index) const;
+};
+
+#endif
