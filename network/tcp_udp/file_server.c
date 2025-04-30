00cfa4d78c902e081e97e712bc2b735eefa87165
qwer1234566666 <dbwlsgh612859@gmail.com>
2025-04-30 10:30:45 +0900

    file

--- network/tcp_udp/file_server.c
+++ network/tcp_udp/file_server.c
@@ -1,6 +1,3 @@
-// ./hello_server
-// ./hello_client 127.0.0.1 8888
-
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,14 +6,16 @@
 #include <unistd.h>
 void error_handling(char *message);
 
-#define BUF_SIZE 1024
+#define BUF_SIZE 30
 
 int main(int argc, char *argv[])
 {
     int serv_sock;
     int clnt_sock;
     int str_len;
-    char message[BUF_SIZE];
+    FILE *fp;
+    char buf[BUF_SIZE];
+    int read_cnt;
 
     struct sockaddr_in serv_addr;
     struct sockaddr_in clnt_addr;
@@ -32,6 +31,8 @@ int main(int argc, char *argv[])
     if (serv_sock == -1)
         error_handling("socker() 만들기 실패");
 
+    fp = fopen("/home/yjh/kubig2025/network/tcp_udp/file_server.txt", "rb");
+
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); // localhost 내부 ip.loopback
@@ -43,23 +44,33 @@ int main(int argc, char *argv[])
         error_handling("바인드 에러!!!");
     if (listen(serv_sock, 5) == -1)
         error_handling("리슨 에러"); // 대기!!!
-    for (int i = 0; i < 20; ++i)
+
+    clnt_addr_size = sizeof(clnt_addr);
+    clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
+    if (clnt_sock == -1)
+        error_handling("accept() 에러!!");
+    else
+        printf("Conneted client : %s \n", inet_ntoa(clnt_addr.sin_addr));
+
+    while (1)
     {
-        clnt_addr_size = sizeof(clnt_addr);
-        clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
-        if (clnt_sock == -1)
-            error_handling("accept() 에러!!");
-        else
-            printf("Conneted client %d : %s \n", i + 1, inet_ntoa(clnt_addr.sin_addr));
-        while (str_len = read(clnt_sock, message, sizeof(message) - 1))
+        read_cnt = fread((void *)buf, 1, BUF_SIZE, fp);
+        printf("outdata cnt \n");
+        if (read_cnt < BUF_SIZE)
         {
-            message[str_len] = '\0';
-            printf("서버 받은 메세지: %s \n", message);
-            write(clnt_sock, message, str_len);
+            write(clnt_sock, buf, read_cnt);
+            break;
         }
-        close(clnt_sock);
+        write(clnt_sock, buf, BUF_SIZE);
     }
 
+    // 우아한 종료
+    shutdown(clnt_sock, SHUT_WR);
+    read(clnt_sock, buf, BUF_SIZE);            // sendQ 종료!! recvQ 살아 있음.
+    printf("Message from client: %s \n", buf); // sendQ 종료!! recvQ 살아 있음.
+
+    fclose(fp);
+    close(clnt_sock);
     close(serv_sock);
 
     return 0;
