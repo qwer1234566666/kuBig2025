1bdbcaf24d122d8f592402dbd2f9cdfa9eb4a52a
qwer1234566666 <dbwlsgh612859@gmail.com>
2025-05-09 09:03:58 +0900

    python

--- python/a20_tuple.py
+++ python/a20_tuple.py
@@ -4,7 +4,8 @@ def afun():
     return a, b
 
 def main():
-    # iterable 하고 list 와 차이가 없다.
+    # iterable 하고 list 와 거의 차이가 없다.
+    # tuple -> 불변 자료형!
     a = tuple()
     c = 123
     b = a, c
---
+++ python/a21_cPython_import.py
@@ -0,0 +1,4 @@
+import mymodule
+
+h = mymodule.Hello("sukil")
+print(h.greet())
