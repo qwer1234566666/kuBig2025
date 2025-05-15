89bef364f6b547ffd65914982dc2557b9fab3994
qwer1234566666 <dbwlsgh612859@gmail.com>
2025-05-15 16:15:48 +0900

    python

--- python/gravity_pygame/src/gravity/game.py
+++ python/gravity_pygame/src/gravity/game.py
@@ -28,8 +28,9 @@ class Ball(pygame.sprite.Sprite):
 
     def update(self):
         self.gravity()
+        self.collide()
         self.vel -= self.acc
-        if self.vel.length() > 50:
+        if self.vel.length() > 30:
             self.vel = Vec(random.randint(-5, 5), random.randint(-5, 5))
         if self.vel.length() < 5:
             self.vel = Vec(random.randint(-5, 5), random.randint(-5, 5))
@@ -56,6 +57,31 @@ class Ball(pygame.sprite.Sprite):
                 except ZeroDivisionError:
                     pass
 
+    def collide(self):
+        other = pygame.sprite.spritecollide(self, self.game.all_sprite, False)
+        if len(other) > 1:
+            temp = self.rect.center
+            self.vel = (self.vel * self.size + other[1].vel * other[1].size) / (
+                self.size + other[1].size
+            )
+            self.size = self.size + other[1].size
+            if self.size > 100:
+                self.size = 100
+            self.image = pygame.Surface((self.size * 2, self.size * 2))
+            self.color = (
+                random.randint(0, 255),
+                random.randint(0, 255),
+                random.randint(0, 255),
+            )
+            pygame.draw.circle(
+                self.image, self.color, (self.size, self.size), self.size
+            )
+            self.image.set_colorkey((0, 0, 0))
+            self.rect = self.image.get_rect()
+            self.rect.center = temp
+            other[1].kill()
+            del other[1]
+
 
 class Game:
     def __init__(self):
@@ -67,7 +93,7 @@ class Game:
         self.all_sprite = pygame.sprite.Group()
 
     def update(self):
-        if len(self.all_sprite) < 10:
+        if len(self.all_sprite) < 6:
             ball = Ball(self)
             self.all_sprite.add(ball)
         self.all_sprite.update()
