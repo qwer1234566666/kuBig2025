e218d9fddf8d3f8a6f669b83df3949fee792abb3
qwer1234566666 <dbwlsgh612859@gmail.com>
2025-06-02 10:29:46 +0900

    opencv

---
+++ opencv/part5/43_hog.cpp
@@ -0,0 +1,36 @@
+#include </home/yjh/kubig2025/opencv/part2/color.hpp>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <vector>
+
+using namespace std;
+using namespace cv;
+
+String folderPath = "/home/yjh/kubig2025/opencv/data/";
+
+int main()
+{
+    VideoCapture cap(folderPath + "vtest.avi");
+
+    HOGDescriptor hog;
+    hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
+
+    vector<Rect> people;
+    Mat img;
+    while (true)
+    {
+        cap >> img;
+        if (img.empty())
+            break;
+        hog.detectMultiScale(img, people);
+        for (auto &rect : people)
+        {
+            rectangle(img, rect, red);
+        }
+        imshow("img", img);
+        if (waitKey(100) == 27)
+            break;
+    }
+    destroyAllWindows();
+    return 0;
+}
