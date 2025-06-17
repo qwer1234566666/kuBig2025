33f5453856dfd9f183011b61b3ed476af871c2d8
qwer1234566666 <dbwlsgh612859@gmail.com>
2025-06-17 17:11:11 +0900

    aa

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -44,7 +44,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/yjh/kubig2025/build/opencv/part6/cmake_install.cmake")
+  include("/home/yjh/kubig2025/build/opencv/part7/cmake_install.cmake")
   include("/home/yjh/kubig2025/build/ml_dl_python/dl_module/cmake_install.cmake")
 
 endif()
