6d5f2ce000c238e56f7b75e6136707ca5dd09380
qwer1234566666 <dbwlsgh612859@gmail.com>
2025-06-17 14:05:17 +0900

    zmp

--- ml_dl_python/dl_module/zmq_client.cpp
+++ ml_dl_python/dl_module/zmq_client.cpp
@@ -1,4 +1,37 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <zmq.hpp>
+
+using namespace std;
+using namespace zmq;
+
 int main()
 {
+    context_t context(1);
+    socket_t requester(context, socket_type::req);
+    const char *ipc_file_path = "/tmp/zmq_server";
+    requester.connect("ipc://" + string(ipc_file_path));
+    cout << "클라이언트가 IPC 소켓에 연결되었습니다: " << ipc_file_path << endl;
+    int request_count = 0;
+    while (true)
+    {
+        string request_str = "클라이언트에서 간다~ " + to_string(request_count++);
+        message_t request_msg(request_str.begin(), request_str.end());
+        recv_result_t result = requester.send(request_msg, send_flags::none);
+        message_t reply_msg;
+        recv_result_t reply_result = requester.recv(reply_msg, recv_flags::none);
+
+        if (reply_result && reply_result.value() > 0)
+        {
+            string reply_str = reply_msg.to_string();
+            cout << "서버로부터 받은 메시지: " << reply_str << endl;
+        }
+        else
+        {
+            cerr << "서버로부터 응답을 받지 못했습니다." << endl;
+        }
+        sleep(2);
+    }
     return 0;
 }
--- ml_dl_python/dl_module/zmq_server.cpp
+++ ml_dl_python/dl_module/zmq_server.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <string>
+#include <unistd.h>
 #include <zmq.hpp>
+
 using namespace std;
+using namespace zmq;
+
 int main()
 {
-    cout << "ZMQ Server is running..." << endl;
+    context_t context(1);
+    socket_t responder(context, socket_type::rep);
+    const char *ipc_file_path = "/tmp/zmq_server";
+    if (unlink(ipc_file_path) == -1 && errno != ENOENT)
+    {
+        cerr << "에러 IPC 파일 삭제: " << strerror(errno) << endl;
+        return 1;
+    }
+    responder.bind("ipc://" + string(ipc_file_path));
+    cout << "서버가 IPC 소켓에 바인딩되었습니다: " << ipc_file_path << endl;
+    while (true)
+    {
+        message_t request;
+        recv_result_t result = responder.recv(request, recv_flags::none);
+        if (result && result.value() > 0)
+        {
+            string received_msg = request.to_string();
+            cout << "받은 메시지: " << received_msg << endl;
+            string reply_msg = "메시지 수신됨: " + received_msg; // echo 메시지
+            responder.send(message_t(reply_msg), send_flags::none);
+        }
+    }
     return 0;
 }
